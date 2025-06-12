575adb77d584cd9e82bcbd18def53949f67c1474
Nicola Stack <nicolastack16@gmail.com>
2025-06-12 15:40:34 +0100

    Setting up dynamoDB

---
+++ dynamo.js
@@ -0,0 +1,31 @@
+const { DynamoDBClient, ScanCommand } = require("@aws-sdk/client-dynamodb");
+
+require('dotenv').config();
+
+const dynamoClient = new DynamoDBClient({ region: process.env.AWS_DEFAULT_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  }
+});
+
+
+const TABLE_NAME = "k9finder";
+
+
+const getUsers = async () => {
+  const params = {
+    TableName: TABLE_NAME
+  };
+
+  try {
+    const users = await dynamoClient.send(new ScanCommand(params));
+    console.log("Users retrieved:", users.Items);
+    return users.Items;
+  } catch (error) {
+    console.error("Error getting users:", error);
+    throw new Error("Could not get users");
+  }
+};
+
+getUsers();
