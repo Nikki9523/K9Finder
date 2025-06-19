2b3e76b4ae51f9c7daf94b50d6ca4991bf42aef2
Nicola Stack <nicolastack16@gmail.com>
2025-06-19 15:09:03 +0100

    adding esbuild to be able to upload to lambda

---
+++ build.js
@@ -0,0 +1,9 @@
+//https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/serverless-sam-cli-using-build-typescript.html
+
+require('esbuild').build({
+  entryPoints: ['index.js'],
+  bundle: true,
+  platform: 'node',
+  target:'node22',
+  outfile: 'dist/index.js',
+}).catch(() => process.exit(1));
