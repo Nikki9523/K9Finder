2288b4abdfbda9efebcfc3a897653a05d24bd4e0
Nicola Stack <nicolastack16@gmail.com>
2025-06-25 16:24:48 +0100

    remove credentials . Lambda uses iam role

--- dynamo.js
+++ dynamo.js
@@ -2,12 +2,7 @@ const { DynamoDBClient, ScanCommand } = require("@aws-sdk/client-dynamodb");
 
 require('dotenv').config();
 
-const dynamoClient = new DynamoDBClient({ region: process.env.AWS_DEFAULT_REGION,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-  }
-});
+const dynamoClient = new DynamoDBClient({ region: process.env.AWS_DEFAULT_REGION });
 
 
 const TABLE_NAME = "k9finder";
