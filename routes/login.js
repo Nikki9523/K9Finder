7bdde596cbc1113ae6a988b061f586f57767f8fa
Nicola Stack <nicolastack16@gmail.com>
2025-08-14 17:22:16 +0100

    extract login logic into controller

--- routes/login.js
+++ routes/login.js
@@ -1,25 +1,7 @@
 const express = require("express");
+const loginController = require("../controllers/loginController");
 const router = express.Router();
-const { validateCognitoUserAndPassword } = require("../cognito.js");
 
-router.post("/", async (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    return res.status(400).json({
-      message: "Please provide both email and password for authentication",
-    });
-  }
-  try {
-    const token = await validateCognitoUserAndPassword(email, password);
-    return res.status(200).json({ message: "Login was successful", token });
-  } catch (error) {
-    console.error("Error during login:", error);
-    if (error.message === "Invalid email or password") {
-      return res.status(401).json({ message: "Invalid email or password" });
-    } else {
-      return res.status(500).json({ message: "Internal server error" });
-    }
-  }
-});
+router.post("/", loginController.login);
 
 module.exports = router;
